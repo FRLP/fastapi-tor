cb3ded213769ec50954229b217b76771ba1ab3da
forelps <dev.forelps@gmail.com>
2024-07-30 22:19:00 +0900

    change: start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y tor
+sudo apt-get update
+sudo apt-get install -y tor
 
-echo "SocksPort 0.0.0.0:9050" > /etc/tor/torrc
-echo "ControlPort 0.0.0.0:9051" >> /etc/tor/torrc
+sudo sh -c 'echo "SocksPort 0.0.0.0:9050" > /etc/tor/torrc'
+sudo sh -c 'echo "ControlPort 0.0.0.0:9051" >> /etc/tor/torrc'
 
-tor &
+sudo systemctl start tor
 
 pip install -r requirements.txt
 uvicorn main:app --host 0.0.0.0 --port 8000
